daeb4def0a3db55cf607ed4a4a0889193dd08b6f
Ogie Galicia <o.galicia.cpa@gmail.com>
2022-11-06 11:08:38 +0800

    Feat: add implicit conversion to string
    - to fix issue #5

--- src/MongoSharpen/Core/CreatedBy.cs
+++ src/MongoSharpen/Core/CreatedBy.cs
@@ -2,4 +2,15 @@
 
 public sealed class CreatedBy : Entity
 {
+    public CreatedBy()
+    {
+    }
+
+    private CreatedBy(string value)
+    {
+        Id = value;
+    }
+
+    public static implicit operator CreatedBy(string value) => new(value);
+    public static implicit operator string(CreatedBy value) => value.Id;
 }
--- src/MongoSharpen/Core/DeletedBy.cs
+++ src/MongoSharpen/Core/DeletedBy.cs
@@ -2,4 +2,15 @@ namespace MongoSharpen;
 
 public sealed class DeletedBy : Entity
 {
+    public DeletedBy()
+    {
+    }
+
+    private DeletedBy(string value)
+    {
+        Id = value;
+    }
+
+    public static implicit operator DeletedBy(string value) => new(value);
+    public static implicit operator string(DeletedBy value) => value.Id;
 }
